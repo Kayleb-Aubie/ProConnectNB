d3e907a86ff635249c1ec600af8eeae1e59a27c4
Kayleb Aubie Boudreau <boudreaukayleb@gmail.com>
2026-01-16 10:58:51 -0400

    logstream added

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,14 +6,13 @@ builder.Services.AddControllers(); // Ajout des controllers automatique (API end
 //(Scoped = une instance par requete http)
 builder.Services.AddScoped<UserService>(); // Enregistrement du service UserService pour l'injection de dependance car se nest pas un controller donc pas fait automatique par le framework
 
+builder.Logging.ClearProviders(); // Nettoyage des providers de logging par defaut
+builder.Logging.AddConsole(); // Ajout du provider de logging pour la console
+
 WebApplication app = builder.Build(); // Construction de l'application (apres avoir configurer les services et le builder)
 
 app.MapControllers(); // Mappe les controllers (endpoints) pour l'application
 
 app.UseDeveloperExceptionPage(); // Page d'exception detaillee pour le developpement (affiche les erreurs dans le navigateur)
-
-//app.UseRouting(); // Active le routage des requetes HTTP vers les endpoints (controllers)
-
-//app.UseAuthorization(); // Middleware d'autorisation (verifie les droits d'acces aux ressources)
 
 app.Run(); // Demarre l'application et ecoute les requetes HTTP entrantes (roulement continu)
